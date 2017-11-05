1c065ed344309e737d283bfe430086c9445fff61
Administrator <Administrator@User-PC>
2017-11-05 16:30:32 +0000

    creating the project

---
+++ src/com/learn/abstractclass/AbstractClass.java
@@ -0,0 +1,13 @@
+package com.learn.abstractclass;
+
+public class AbstractClass {
+
+	public int calSuqare(int a)
+	{
+		int square = a*a;
+		return square;
+	}
+	public int calCube(int b);
+	
+	
+}
